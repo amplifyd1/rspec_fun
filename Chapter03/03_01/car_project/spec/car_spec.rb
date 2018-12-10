1e22c1b8b27de02ec92629e7bc0322d003b6b7af
wesley sprague <wesleysprague@yahoo.com>
2018-12-10 17:27:53 -0500

    finialized and ran tests for car model

--- Chapter03/03_01/car_project/spec/car_spec.rb
+++ Chapter03/03_01/car_project/spec/car_spec.rb
@@ -5,24 +5,46 @@ describe 'Car' do
     it "allows reading and writing for :make" do
       car = Car.new
       car.make = 'Test'
-      expect car.make.to eq('Test')
+      expect(car.make).to eq('Test')
     end
 
     it "allows reading and writing for :year" do
       car = Car.new
       car.year = 1999
-      expect car.make.to eq(1999)
+      expect(car.year).to eq(1999)
     end
 
     it "allows reading and writing for :color" do
       car = Car.new
       car.color = 'blue'
-      expect car.make.to eq('blue')
+      expect(car.color).to eq('blue')
+    end
+
+    it "allows reading for wheels" do
+      car = Car.new
+      expect(car.wheels).to eq(4)
     end
 
   describe '.color' do
-
+    it "returns an array of color names" do
+      c = ['blue', 'black', 'red', 'green']
+      expect(Car.colors).to match_array(c)
+    end
+  end
 
   describe '#full_name' do
+    it "returns a string in the expected format" do
+      @chevy = Car.new(make: 'chevy', year: 2004, color: 'blue')
+      expect(@chevy.full_name).to eq('2004 chevy (blue)')
+    end
+  end
+
+    context "when initialized with no arguments" do
+      it "returns a string  using default values" do
+        car = Car.new
+        expect(car.full_name).to eq('2007 Volvo (unknown)')
+      end
+    end
+  end
 end
 
